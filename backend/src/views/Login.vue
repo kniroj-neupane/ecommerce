c3b3445590cedabf88108ed29163f62279d676e8
Niroj Neupane Khatri <knirojneupane@gmail.com>
2024-02-21 16:29:11 +0545

    Add sign up and registration pages

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -1,14 +1,49 @@
 <template>
-    <h1>Login Form</h1>
-  </template>
-  
-  <script>
-    export default {
-      // Your script goes here
-    }
-  </script>
-  
-  <style scoped>
-    /* Your styles go here */
-  </style>
-  
+  <GuestLayout title="Sign in to your account">
+    <form class="space-y-6" @submit.prevent="login" method="POST">
+      <div>
+      <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+      <div class="mt-2">
+        <input id="email" name="email" type="email" autocomplete="email" required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" />
+      </div>
+    </div>
+
+    <div>
+      <div class="flex items-center justify-between">
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        <div class="text-sm">
+          <a href="request-password" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+        </div>
+      </div>
+      <div class="mt-2">
+        <input id="password" name="password" type="password" autocomplete="current-password" required=""
+          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" />
+      </div>
+    </div>
+
+    <div>
+      <button type="submit"
+        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+        in</button>
+    </div>
+    </form>
+
+</GuestLayout>
+</template>
+
+<script setup>
+import GuestLayout from '../components/GuestLayout.vue';
+import store from '../store';
+import router from '../router';
+function login()
+{
+  store.dispatch('login',user)
+    .then(()=>{
+      router.push({name: 'app.dashboard'});
+    })
+    .catch(({response})=>{
+      console.log(response);
+    })
+}
+</script>
