27b4e9c5c9ac68023bc2a88b46f43662f4c94490
kniroj-neupane <knirojneupane@gmail.com>
2024-02-29 16:34:54 +0545

    Install Breeze

---
+++ resources/js/Components/Example.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello Vue in Laravel!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
+
+<style scoped>
+/* Your component-specific styles */
+</style>
