1f8f7ebd11ba544de7810b8ad74bba5ca877d4e4
kniroj-neupane <knirojneupane@gmail.com>
2024-03-10 16:41:17 +0545

    Add simple cart function in frontend

--- resources/js/Pages/Products.vue
+++ resources/js/Pages/Products.vue
@@ -11,19 +11,28 @@
     ],
   }
   ```
---> 
+-->
+
 <template>
   <div class="bg-white dark:bg-gray-800">
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 class="sr-only">Products</h2>
 
       <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a v-for="product in state.products" :key="product.id"  class="group">
+        <a v-for="product in state.products" :key="product.id" class="group">
           <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img :src="product.image_url" :alt="product.title" class="h-full w-full object-cover object-center group-hover:opacity-75" />
+            <img :src="product.image_url" :alt="product.title"
+              class="h-full w-full object-cover object-center group-hover:opacity-75" />
           </div>
-          <h3 class="mt-4 text-sm text-gray-700 dark:text-gray-100">{{ product.title }}</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900 dark:text-gray-100">$ {{ product.price }}</p>
+          <div class="flex justify-around items-center">
+            <div>
+              <h3 class="mt-4 text-sm text-gray-700 dark:text-gray-100">{{ product.title }}</h3>
+              <p class="mt-1 text-lg font-medium text-gray-900 dark:text-gray-100">$ {{ product.price }}</p>
+            </div>
+            <div>
+              <PrimaryButton @click="addToCart(product)">Add to cart</PrimaryButton>
+            </div>
+          </div>
         </a>
       </div>
     </div>
@@ -32,6 +41,7 @@
 
 <script setup>
 import { onMounted, reactive } from 'vue';
+import PrimaryButton from '../Components/PrimaryButton.vue';
 const products1 = [
   {
     id: 1,
@@ -69,12 +79,18 @@ const products1 = [
 ]
 const state = reactive({
   products: [],
+  cartItems:[],
 });
 onMounted(() => {
-    axios.get('api/products')
-        .then(response=>{
-    console.log(response.data.data);
-    state.products = response.data.data;
+  axios.get('api/products')
+    .then(response => {
+      console.log(response.data.data);
+      state.products = response.data.data;
+    })
 })
-})
+function addToCart(product){
+  state.cartItems.push(product);
+  console.log(state.cartItems);
+  
+}
 </script>
